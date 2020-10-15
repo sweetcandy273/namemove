41a213f81709718d3698deb25f01197e525fcdd4
Pongsavet Phukaet <Pongsavet_name@hotmail.com>
2020-10-15 22:11:55 +0700

    show Name shortstory and yesr

--- js/index.js
+++ js/index.js
@@ -1,20 +1,19 @@
-
+var firebaseConfig = {
+  apiKey: "AIzaSyACZWyH6CUeqZuv7bM9ZcDzluXkXQz6cH4",
+  authDomain: "namemove-ad05c.firebaseapp.com",
+  databaseURL: "https://namemove-ad05c.firebaseio.com",
+  projectId: "namemove-ad05c",
+  storageBucket: "namemove-ad05c.appspot.com",
+  messagingSenderId: "612382199116",
+  appId: "1:612382199116:web:c56c4e1d3ceb81da9b93e9",
+  measurementId: "G-F2J8PYKL22"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+var db = firebase.firestore();
 $(function () {
 
-  var firebaseConfig = {
-    apiKey: "AIzaSyACZWyH6CUeqZuv7bM9ZcDzluXkXQz6cH4",
-    authDomain: "namemove-ad05c.firebaseapp.com",
-    databaseURL: "https://namemove-ad05c.firebaseio.com",
-    projectId: "namemove-ad05c",
-    storageBucket: "namemove-ad05c.appspot.com",
-    messagingSenderId: "612382199116",
-    appId: "1:612382199116:web:c56c4e1d3ceb81da9b93e9",
-    measurementId: "G-F2J8PYKL22"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-  var db = firebase.firestore();
 
   db.collection("movies").get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
@@ -26,35 +25,38 @@ $(function () {
       $("#list-movie").append(result)
     });
   });
-  // firebase.auth().onAuthStateChanged(function(user) {
-  //     if (user) {
-  //       // User is signed in.
-  //      var email = user.email;
-  //      console.log(`User with email ${email} signed in`);
-  //       window.location.href = "index.html"
-  //     } else {
-  //       // User is signed out.
-  //       window.location.href = "signin.html"
-  //     }
-  //   });
 
-
-
+  db.collection("movies").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      if(doc.data().type == "comedy"){
+        const result = 
+        `<ons-carousel-item modifier="nodivider" id="${doc.data().title}" onclick="openMovieDetails(this.id)">
+          <img src="${doc.data().posterURL}">
+        </ons-carousel-item>`
+        $("#list-movie-comedy").append(result)
+      }
+    });
+  });
 })
 
 
 document.addEventListener('init', function (event) {
   var page = event.target;
-   if (page.id === 'movie_details') {
-    page.querySelector('#movie_title').innerHTML = page.data.title;
-    page.querySelector('#movie_year').innerHTML = page.data.year;
-    page.querySelector('#movie_movie_synopsis').innerHTML = page.data.shortstory;
-  }
+
 
 });
 
 function openMovieDetails(id) {
-  document.querySelector('#myNavigator').pushPage('views/movie_details.html', { data: { title: id } });
+  db.collection("movies").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        if(doc.data().title == id){
+        $("#movie_title").append(doc.data().title)
+        $("#movie_year").append(doc.data().year)
+        $(".movie_synopsis").append(doc.data().shortstory)
+        }
+    });
+  });
+  document.querySelector('#myNavigator').pushPage('views/movie_details.html');
 }
 
 function goBack() {
