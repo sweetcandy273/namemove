a842113ac3f0d606b27dabcacc824b1ed02cf7fd
ratchaya166 <ratchaya166@gmail.com>
2020-10-15 19:24:07 +0700

    add  firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCqPjLCjkfLohoLgx-dU_9Hwr2-6rYOjeo",
+  authDomain: "mobileapp-f5337.firebaseapp.com",
+  databaseURL: "https://mobileapp-f5337.firebaseio.com",
+  projectId: "mobileapp-f5337",
+  storageBucket: "mobileapp-f5337.appspot.com",
+  messagingSenderId: "6796166923",
+  appId: "1:6796166923:web:8f97be0e7ab56dd32e040e",
+  measurementId: "G-P57FQ6754Q",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
