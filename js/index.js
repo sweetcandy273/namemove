009c4410e727b3c2530205d89232870a1ac13984
Pongsavet Phukaet <Pongsavet_name@hotmail.com>
2020-10-15 18:59:34 +0700

    Edit splitter

--- js/index.js
+++ js/index.js
@@ -1,115 +1,99 @@
 
-$(function(){
+$(function () {
 
   var firebaseConfig = {
-    apiKey: "AIzaSyDOCAbC123dEf456GhI789jKl01-MnO",
-    authDomain: "myapp-project-123.firebaseapp.com",
-    databaseURL: "https://myapp-project-123.firebaseio.com",
-    projectId: "myapp-project-123",
-    storageBucket: "myapp-project-123.appspot.com",
-    messagingSenderId: "65211879809",
-    appId: "1:65211879909:web:3ae38ef1cdcb2e01fe5f0c",
-    measurementId: "G-8GSGZQ44ST"
+    apiKey: "AIzaSyACZWyH6CUeqZuv7bM9ZcDzluXkXQz6cH4",
+    authDomain: "namemove-ad05c.firebaseapp.com",
+    databaseURL: "https://namemove-ad05c.firebaseio.com",
+    projectId: "namemove-ad05c",
+    storageBucket: "namemove-ad05c.appspot.com",
+    messagingSenderId: "612382199116",
+    appId: "1:612382199116:web:c56c4e1d3ceb81da9b93e9",
+    measurementId: "G-F2J8PYKL22"
   };
-      // Initialize Firebase
-      firebase.initializeApp(firebaseConfig);
-      firebase.analytics();
-      var db = firebase.firestore();
-    
-        db.collection("movies").get().then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-         
-          // var row = `
-          // <ons-carousel-item modifier="nodivider" id="${doc.data().title}" onclick="openMovieDetails(this.id)">
-          //   <img src="${doc.data().posterURL}" >
-          // </ons-carousel-item>
-      
-          // `
-        
-              var row = `<div class="card">
-            <img class="card-img-top" src="${doc.data().posterURL}" alt="">
-            <div class="card-body">
-            <h4 class="card-title">
-                ${doc.data().title}(${doc.data().year})
-            </h4>
-            <p class="card-text">  ${doc.data().shortstory}       </p>
-         </div>
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  var db = firebase.firestore();
 
-            `
-           
-            $("#list").append(row);
-        });
+  db.collection("movies").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+
+      // var row = `
+      // <ons-carousel-item modifier="nodivider" id="${doc.data().title}" onclick="openMovieDetails(this.id)">
+      //   <img src="${doc.data().posterURL}" >
+      // </ons-carousel-item>
+
+      // `
+
+      // var row = `<div class="card">
+      //       <img class="card-img-top" src="${doc.data().posterURL}" alt="">
+      //       <div class="card-body">
+      //       <h4 class="card-title">
+      //           ${doc.data().title}(${doc.data().year})
+      //       </h4>
+      //       <p class="card-text">  ${doc.data().shortstory}       </p>
+      //    </div>
+
+      //       `
+
+      // $("#list").append(row);
     });
-    // firebase.auth().onAuthStateChanged(function(user) {
-    //     if (user) {
-    //       // User is signed in.
-    //      var email = user.email;
-    //      console.log(`User with email ${email} signed in`);
-    //       window.location.href = "index.html"
-    //     } else {
-    //       // User is signed out.
-    //       window.location.href = "signin.html"
-    //     }
-    //   });
-      
-    
+  });
+  // firebase.auth().onAuthStateChanged(function(user) {
+  //     if (user) {
+  //       // User is signed in.
+  //      var email = user.email;
+  //      console.log(`User with email ${email} signed in`);
+  //       window.location.href = "index.html"
+  //     } else {
+  //       // User is signed out.
+  //       window.location.href = "signin.html"
+  //     }
+  //   });
+
+
 
 })
 
-document.addEventListener('init', function(event) {
-    var page = event.target;
-    
-    if (page.id === 'home') {
-      page.querySelector('#menu_button').onclick = function() {
-        document.querySelector('#menu').open();
-      };
-  
-      //The postchange event listener is used for changing the dots when the carousel is changed
-  
-      page.querySelector('#carousel').addEventListener("postchange", function() {
-        page.querySelector('#dot0').classList.remove("circle_current");
-        page.querySelector('#dot1').classList.remove("circle_current");
-        page.querySelector('#dot2').classList.remove("circle_current");
-  
-        page.querySelector('#dot' + page.querySelector('#carousel').getActiveIndex()).classList.add("circle_current");
-  
-      });
-    }
-     else if (page.id === 'movie_details') {
-      page.querySelector('#movie_title').innerHTML = page.data.title;
-    }
-   
-  });
-  
-  
-  
+document.addEventListener('init', function (event) {
+  var page = event.target;
+  console.log(page.id);
+
+
+});
 
 
 
-  function openMovieDetails(id) {
-    document.querySelector('#myNavigator').pushPage('views/movie_details.html', {data: {title: id}});
-  }
-  
-  function openHome() {
-    document.querySelector('#myNavigator').pushPage('views/home_splitter.html');
-  }
-  function goBack() {
-    document.querySelector('#menu').close().then(function() {
-      document.querySelector('#myNavigator').popPage()
-    });
-  }
-  function goaction() {
-    document.querySelector('#myNavigator').pushPage('views/action.html');
-  }
-  function gocomedy() {
-    document.querySelector('#myNavigator').pushPage('views/comedy.html');
-  }
-  function gohorror() {
-    document.querySelector('#myNavigator').pushPage('views/horror.html');
-  }
-  function romance() {
-    document.querySelector('#myNavigator').pushPage('views/romance.html');
-  }
-// `<h3>${doc.data().title}</h3>`
- 
+
+
+
+
+function openMovieDetails(id) {
+  document.querySelector('#myNavigator').pushPage('views/movie_details.html', { data: { title: id } });
+}
+
+// function openHome() {
+//   document.querySelector('#myNavigator').pushPage('views/home_splitter.html');
+// }
+
+// function goBack() {
+//   document.querySelector('#menu').close().then(function () {
+//     document.querySelector('#myNavigator').popPage()
+//   });
+// }
+// function goaction() {
+//   document.querySelector('#myNavigator').pushPage('views/action_splitter.html');
+// }
+// function gocomedy() {
+//   document.querySelector('#myNavigator').pushPage('views/comedy.html');
+// }
+// function gohorror() {
+//   document.querySelector('#myNavigator').pushPage('views/horror.html');
+// }
+// function romance() {
+//   document.querySelector('#myNavigator').pushPage('views/romance.html');
+// }
+// // `<h3>${doc.data().title}</h3>`
+
 
