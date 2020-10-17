34fbb4b3baea0e5b97f65bdc02faaef43ea4a1cf
Sweetcandy273 <sweetcandy.aum@gmail.com>
2020-10-17 23:02:10 +0700

    edit function search susuna

--- js/index.js
+++ js/index.js
@@ -151,7 +151,7 @@ function openMovieDetails(id) {
         db.collection("movies").get().then((querySnapshot) => {
           querySnapshot.forEach((doc) => {
             const result5 =
-              `<ons-carousel-item modifier="nodivider" id="${doc.data().title}" >
+              `<ons-carousel-item modifier="nodivider" id="${doc.data().title}" onclick="openMovieDetails(this.id)">
                 <img src="${doc.data().posterURL}">
               </ons-carousel-item>`
 
@@ -189,28 +189,80 @@ function goBack() {
 // // `<h3>${doc.data().title}</h3>`
 
 
-//AUM ZONE
+//search ZONE
 
 
 
-function page_search() {  
+function page_search() {
   $("#sug_show").empty();
   $("#search_show").empty();
+
+
+          var alert_search = `<p style="text-center">รายการแนะนำ</p>`;
+
+          $("#sug_show").append(alert_search);
+
   db.collection("movies").get().then((querySnapshot) => {
 
     querySnapshot.forEach((doc) => {
+
+      if (`${doc.data().star}` > 4.5) {
+        var star = ` 
+<ons-icon style="color: red" icon="fa-star"></ons-icon>
+<ons-icon style="color: red" icon="fa-star"></ons-icon>
+<ons-icon style="color: red" icon="fa-star"></ons-icon>
+<ons-icon style="color: red" icon="fa-star"></ons-icon>
+<ons-icon style="color: red" icon="fa-star"></ons-icon>`
+
+      } else if (`${doc.data().star}` > 3.5) {
+        var star = ` 
+<ons-icon style="color: red" icon="fa-star"></ons-icon>
+<ons-icon style="color: red" icon="fa-star"></ons-icon>
+<ons-icon style="color: red" icon="fa-star"></ons-icon>
+<ons-icon style="color: red" icon="fa-star"></ons-icon>
+<ons-icon style="color: gray" icon="fa-star"></ons-icon>`
+
+      } else if (`${doc.data().star}` > 2.5) {
+        var star = ` 
+<ons-icon style="color: red" icon="fa-star"></ons-icon>
+<ons-icon style="color: red" icon="fa-star"></ons-icon>
+<ons-icon style="color: red" icon="fa-star"></ons-icon>
+<ons-icon style="color: gray" icon="fa-star"></ons-icon>
+<ons-icon style="color: gray" icon="fa-star"></ons-icon>`
+
+      } else if (`${doc.data().star}` > 1.5) {
+        var star = ` 
+<ons-icon style="color: red" icon="fa-star"></ons-icon>
+<ons-icon style="color: red" icon="fa-star"></ons-icon>
+<ons-icon style="color: gray" icon="fa-star"></ons-icon>
+<ons-icon style="color: gray" icon="fa-star"></ons-icon>
+<ons-icon style="color: gray" icon="fa-star"></ons-icon>`
+
+      } else {
+        var star = ` 
+<ons-icon style="color: red" icon="fa-star"></ons-icon>
+<ons-icon style="color: gray" icon="fa-star"></ons-icon>
+<ons-icon style="color: gray" icon="fa-star"></ons-icon>
+<ons-icon style="color: gray" icon="fa-star"></ons-icon>
+<ons-icon style="color: gray" icon="fa-star"></ons-icon>`
+
+      }
+
+
       var row = `
+      <ons-row style="margin: 5px;" id="${doc.data().title}">
+      <ons-col class="text-center">
+      <img src="${doc.data().posterURL}" width="50%" style="margin: 5px 5px;" alt="">
+      </ons-col>
 
-        <ons-row style="margin: 5px;">
-        <ons-col class="text-center">
-        <img src="${doc.data().posterURL}" width="50%" style="margin: 5px 5px;" alt="">
-        </ons-col>
+      <ons-col>
+      <p style="font-size:10px" class="text-center">${doc.data().title} (${doc.data().year})</p>
 
-        <ons-col>
-        <p style="font-size:10px">${doc.data().title} (${doc.data().year})</p>
-        </ons-col>
-      
-        </ons-row>`
+      <p style="font-size:10px" class="text-center">`+ star + ` (${doc.data().star})</p>
+   
+      </ons-col>
+    
+      </ons-row>`
 
       $("#search_show").append(row);
     });
@@ -219,7 +271,7 @@ function page_search() {
 
 
   $("#search").click(function () {
-   
+
     $("#search_show").empty();
     $("#sug_show").empty();
     $("#search_show").append("ผลลัพธ์การค้นหา:");
@@ -230,6 +282,48 @@ function page_search() {
 
       querySnapshot.forEach((doc) => {
 
+        if (`${doc.data().star}` > 4) {
+          var star = ` 
+<ons-icon style="color: red" icon="fa-star"></ons-icon>
+<ons-icon style="color: red" icon="fa-star"></ons-icon>
+<ons-icon style="color: red" icon="fa-star"></ons-icon>
+<ons-icon style="color: red" icon="fa-star"></ons-icon>
+<ons-icon style="color: red" icon="fa-star"></ons-icon>`
+
+        } else if (`${doc.data().star}` > 3) {
+          var star = ` 
+<ons-icon style="color: red" icon="fa-star"></ons-icon>
+<ons-icon style="color: red" icon="fa-star"></ons-icon>
+<ons-icon style="color: red" icon="fa-star"></ons-icon>
+<ons-icon style="color: red" icon="fa-star"></ons-icon>
+<ons-icon style="color: gray" icon="fa-star"></ons-icon>`
+
+        } else if (`${doc.data().star}` > 2) {
+          var star = ` 
+<ons-icon style="color: red" icon="fa-star"></ons-icon>
+<ons-icon style="color: red" icon="fa-star"></ons-icon>
+<ons-icon style="color: red" icon="fa-star"></ons-icon>
+<ons-icon style="color: gray" icon="fa-star"></ons-icon>
+<ons-icon style="color: gray" icon="fa-star"></ons-icon>`
+
+        } else if (`${doc.data().star}` > 1) {
+          var star = ` 
+<ons-icon style="color: red" icon="fa-star"></ons-icon>
+<ons-icon style="color: red" icon="fa-star"></ons-icon>
+<ons-icon style="color: gray" icon="fa-star"></ons-icon>
+<ons-icon style="color: gray" icon="fa-star"></ons-icon>
+<ons-icon style="color: gray" icon="fa-star"></ons-icon>`
+
+        } else {
+          var star = ` 
+<ons-icon style="color: gray" icon="fa-star"></ons-icon>
+<ons-icon style="color: gray" icon="fa-star"></ons-icon>
+<ons-icon style="color: gray" icon="fa-star"></ons-icon>
+<ons-icon style="color: gray" icon="fa-star"></ons-icon>
+<ons-icon style="color: gray" icon="fa-star"></ons-icon>`
+
+        }
+
         var titleforcheck = `${doc.data().title}`;
         // console.log(titleforcheck);
         var yearforcheck = `${doc.data().year}`;
@@ -239,16 +333,21 @@ function page_search() {
         var regexLetter = /[a-zA-z]/;
 
         if (regexLetter.test(search_input)) {
+          
+  
+          if (titleforcheck.toLowerCase().indexOf(search_input.toLowerCase())!=-1) {
 
-          if (titleforcheck.indexOf(search_input) != -1) {
             var row = `
-            <ons-row style="margin: 5px;">
+            <ons-row style="margin: 5px;" id="${doc.data().title}" >
             <ons-col class="text-center">
             <img src="${doc.data().posterURL}" width="50%" style="margin: 5px 5px;" alt="">
             </ons-col>
     
             <ons-col>
-            <p style="font-size:10px">${doc.data().title} (${doc.data().year})</p>
+            <p style="font-size:10px" class="text-center">${doc.data().title} (${doc.data().year})</p>
+
+            <p style="font-size:10px" class="text-center">`+ star + `(${doc.data().star})</p>
+         
             </ons-col>
           
             </ons-row>`
@@ -261,13 +360,16 @@ function page_search() {
 
           if (search_input == yearforcheck) {
             var row = `
-            <ons-row style="margin: 5px;">
+            <ons-row style="margin: 5px;" id="${doc.data().title}">
             <ons-col class="text-center">
             <img src="${doc.data().posterURL}" width="50%" style="margin: 5px 5px;" alt="">
             </ons-col>
     
             <ons-col>
-            <p style="font-size:10px">${doc.data().title} (${doc.data().year})</p>
+            <p style="font-size:10px" class="text-center">${doc.data().title} (${doc.data().year})</p>
+
+            <p style="font-size:10px" class="text-center">`+ star + `(${doc.data().star})</p>
+         
             </ons-col>
           
             </ons-row>`
@@ -275,14 +377,14 @@ function page_search() {
           }
           // else{
           //     $("#search_show").empty();
-          //     $("#search_show").append("ไม่พบหนังในปีที่ค้นหา ลองค้นหาใหม่อีกครั้ง");
+          //     $("#search_show").append("ไม่พบหนังในที่ค้นหา ลองค้นหาใหม่อีกครั้ง");
 
           // }
 
         } else {
           $("#search_show").empty();
 
-          var alert_search=`<p style="margin-left: 20px;">ใส่ข้อมูลเป็น ตัวอักษร หรือ ตัวเลข เท่านั้น!!</p>`;
+          var alert_search = `<p style="margin-left: 20px;">ใส่ข้อมูลเป็น ตัวอักษร หรือ ตัวเลข เท่านั้น!!</p>`;
 
           $("#search_show").append(alert_search);
 
@@ -294,9 +396,11 @@ function page_search() {
 
     });
 
+
+    
   });
 
-
+  
+  
 }
-
 
